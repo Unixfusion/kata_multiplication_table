b18bdac5e2e9e8aa186979e011c0356ac5196d7a
UnixF <unixfusion@gmail.com>
2023-11-04 19:02:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] test = {{1,2,3},{2,4,6},{3,6,9}};
+        System.out.println(multiplicationTable(test));
+    }
+    public static int [][] multiplicationTable(int n){
+        int [][] result = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = (i+1) + j + j*i;
+            }
+        }
+        return result;
+    }
+}
